e9cddde54658c8bd69e77978bac8311cb786cbba
Milena Stepanyan <135169227+MilenaStepanyan@users.noreply.github.com>
2023-06-01 00:05:06 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h3>letter from the future</h3>
+    <h4>HELLO WORLD👽g</h4>
+     <img class="saturn" src="https://cutewallpaper.org/24/saturn-png/file3d-saturnpng-wikimedia-commons.png" alt="" />
+     <img class="alien" src="https://cdn-icons-png.flaticon.com/512/3306/3306604.png" alt="" />
+    </>
+  )
+}
+
+export default App
